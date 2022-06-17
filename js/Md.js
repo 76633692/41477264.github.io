31c392d1dd50ca5e9a0bc190b883425f5b899e72
76633692 <102701659+76633692@users.noreply.github.com>
2022-06-16 23:12:52 -0500

    Add files via upload

---
+++ js/Md.js
@@ -0,0 +1,32 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.title', {
+    duration:3000,
+    origin:'bottom',
+    distance:'-100px'
+});
+window.sr = ScrollReveal();
+
+sr.reveal('.texto', {
+    duration:3900,
+    origin:'left',
+    distance:'-100px'
+});
+
+sr.reveal('.imagen', {
+    duration:3900,
+    origin:'butom',
+    distance:'-100px'
+});
+
+
+sr.reveal('.contenedor-galeria', {
+    duration:1000,
+    rotate:{
+       x:1,
+       y:100,
+     
+   }
+});
+
+
---
+++ js/Pc.js
@@ -0,0 +1,30 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.imagen', {
+    duration:3000,
+    origin:'bottom',
+    distance:'-100px'
+});
+window.sr = ScrollReveal();
+
+sr.reveal('.title', {
+    duration:3900,
+    origin:'bottom',
+    distance:'-100px'
+});
+window.sr = ScrollReveal();
+
+sr.reveal('.texto', {
+    duration:3900,
+    origin:'left',
+    distance:'-100px'
+});
+
+sr.reveal('.contenedor-galeria', {
+    duration:1000,
+    rotate:{
+       x:1,
+       y:180,
+     
+   }
+});
