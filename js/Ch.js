1057d15e8655ba127782393c3b1c009c92ff5c8f
76633692 <102701659+76633692@users.noreply.github.com>
2022-06-16 22:39:02 -0500

    Add files via upload

---
+++ js/Ch.js
@@ -0,0 +1,38 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.title', {
+    duration:3000,
+    origin:'bottom',
+    distance:'-100px'
+});
+window.sr = ScrollReveal();
+
+sr.reveal('.texto', {
+    duration:3900,
+    origin:'left',
+    distance:'-100px'
+});
+
+sr.reveal('.imagen', {
+    duration:3900,
+    origin:'left',
+    distance:'150px'
+});
+
+
+sr.reveal('.card', {
+    duration:4000,
+    origin:'left',
+    distance:'-100px'
+   
+     
+   
+});
+sr.reveal('.contenedor-galeria', {
+    duration:1000,
+    rotate:{
+       x:1,
+       y:180,
+     
+   }
+});
